365dee732d03864155cccd8f6afc1d97390d215d
FIRST_NAME LAST_NAME <gabriel.serrano.salas@outlook.com>
2022-07-19 18:59:46 +0200

    Adding pre-push hook

--- scripts/node/environment-replacements/main.js
+++ scripts/node/environment-replacements/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const fs = require('fs');
 const replacementsConfig = require('./config.json');
 
---
+++ scripts/node/hooks/pre-push.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+
+const validateBranchName = async () => {
+	const branchName = (await exec('git rev-parse --abbrev-ref HEAD')).stdout.trim();
+
+	let isInRemote = true;
+
+	await exec(`git show-branch remotes/origin/${branchName}`).catch(() => {
+		isInRemote = false;
+	});
+
+	if (!isInRemote) {
+		const validBranchPrefix = 'feature|bugfix|hotfix|chore|release';
+		const userStoryPrefix = 'US-';
+		const validBranchesRegex = new RegExp(`^(${validBranchPrefix})/${userStoryPrefix}[\\w.-]+$`);
+		if (!validBranchesRegex.test(branchName)) {
+			const msg = `Branch names in this project must adhere to this contract: (${validBranchPrefix})/${userStoryPrefix}*.`;
+			console.error(msg);
+			process.exit(1);
+		}
+	}
+};
+
+validateBranchName();
