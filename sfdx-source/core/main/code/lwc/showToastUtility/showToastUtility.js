b33f69a0414304e3d76eaa67bdca1f8dacbfb8c0
Gabriel Serrano <gabriel.serrano.salas@outlook.com>
2022-09-01 17:06:40 +0200

    chore: echo

--- sfdx-source/core/main/code/lwc/showToastUtility/showToastUtility.js
+++ sfdx-source/core/main/code/lwc/showToastUtility/showToastUtility.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /**
- * A Toast for notifications test test test
+ * A Toast for notifications test test test test test
  */
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 /**
