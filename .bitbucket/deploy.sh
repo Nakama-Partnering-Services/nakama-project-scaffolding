0a47a178426439075b53d5f4c5a7bbea1c5e5d59
Gabriel Serrano <gabriel.serrano.salas@outlook.com>
2022-09-02 23:53:24 +0200

    chore: --wait 60 for all deployments

--- .bitbucket/deploy.sh
+++ .bitbucket/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 sf plugins install sfdx-git-delta
 mkdir deltas
-echo $1
 LAST_DEPLOYMENT_COMMIT_SHA=$(eval echo \${${1}_LAST_DEPLOYMENT_COMMIT_SHA})
-echo $LAST_DEPLOYMENT_COMMIT_SHA
 sf sgd source delta --source sfdx-source --from $LAST_DEPLOYMENT_COMMIT_SHA --to HEAD --output deltas --ignore .forceignore --generate-delta
 npm install fast-xml-parser
 node scripts/node/environment-replacements/main.js || true
@@ -16,6 +14,6 @@ echo $DESTINATION_SFDX_AUTH_URL > ./DESTINATION_SFDX_AUTH_URL.txt
 sfdx auth:sfdxurl:store --sfdxurlfile ./DESTINATION_SFDX_AUTH_URL.txt --setdefaultusername
 # RUN_TEST_PARAMETER can be uncommented to double also run tests on deployments to integration, however it is redundant with MR and will take longer.
 # - RUN_TEST_PARAMETER=$(if [ $TEST_CLASSES ]; then echo "--testlevel RunSpecifiedTests --runtests $TEST_CLASSES"; else echo ""; fi)
-sfdx force:source:deploy --manifest deltas/package/package.xml --postdestructivechanges deltas/destructiveChanges/destructiveChanges.xml
+sfdx force:source:deploy --wait 60 --manifest deltas/package/package.xml --postdestructivechanges deltas/destructiveChanges/destructiveChanges.xml
 # curl --request PUT --header "PRIVATE-TOKEN: $PIPELINE_ACCESS_TOKEN" \
 # 	"$CI_API_V4_URL/projects/$CI_PROJECT_ID/variables/${1}_LAST_DEPLOYMENT_COMMIT_SHA" --form "value=$CI_COMMIT_SHA"
