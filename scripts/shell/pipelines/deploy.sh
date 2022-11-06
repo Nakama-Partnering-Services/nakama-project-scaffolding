fc45613a967b879d65a55f1c9f52d97e7e4801f0
Gabriel <gserrano@external.mol.hu>
2022-11-06 22:32:48 +0100

    chore: remove sh references and added run test in pr body for github

--- scripts/shell/pipelines/deploy.sh
+++ scripts/shell/pipelines/deploy.sh
@@ -19,9 +19,14 @@ FOLDER=$(echo $1 | tr '[:upper:]' '[:lower:]') # workaround since ${1,,} does no
 cp --recursive specific-environments/$FOLDER/. sfdx-source/ || true
 echo 'y' | sfdx plugins:install nakama-plugin-sfdx
 sfdx nps:package:destructive:versionobsoleteflows --path deltas/destructiveChanges/destructiveChanges.xml
-TARGET_SFDX_AUTH_URL=$(eval echo \${${1}_SFDX_AUTH_URL})
-echo $TARGET_SFDX_AUTH_URL > ./TARGET_SFDX_AUTH_URL.txt
-sfdx auth:sfdxurl:store --sfdxurlfile ./TARGET_SFDX_AUTH_URL.txt --setdefaultusername
+# checking if org is already authenticated, like in github
+auth_orgs=$(sfdx auth:list --json)
+number_of_orgs=`jq '.result | length' <<< $auth_orgs`
+if [ "$number_of_orgs" = 0 ]; then
+	TARGET_SFDX_AUTH_URL=$(eval echo \${${1}_SFDX_AUTH_URL})
+	echo $TARGET_SFDX_AUTH_URL > ./TARGET_SFDX_AUTH_URL.txt
+	sfdx auth:sfdxurl:store --sfdxurlfile ./TARGET_SFDX_AUTH_URL.txt --setdefaultusername# temporary silent fallback for GITHUB which already authenticates
+fi
 if [ "$3" = "validation" ]; then
 	sfdx force:source:deploy --wait 60 --checkonly --manifest deltas/package/package.xml --postdestructivechanges deltas/destructiveChanges/destructiveChanges.xml --verbose $RUN_TEST_PARAMETER --json > results.json
 else
