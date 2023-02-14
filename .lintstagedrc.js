ae716ba764f08b647a245a555d91bd387403913e
Gabriel Serrano Salas <gabriel.serrano.salas@outlook.com>
2023-02-14 11:20:02 +0100

    Update .lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
 	'**/lwc/**/*.js': 'import-sort --write',
 	'**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
-	'**/*.{component,js,page}': 'eslint --fix',
+	'**/{aura,lwc}/**': 'eslint --fix',
 	// '**/lwc/**': 'npm run test:unit:silent .',
-	'**/{classes,trigger}/**/*.{cls,trigger}': (filenames) => {
+	'**/*.{cls,trigger}': (filenames) => {
 		const files = filenames.join(',');
 		return `sfdx scanner:run --pmdconfig config/pmd-ruleset.xml --target "${files}" --engine pmd --format table --severity-threshold 3`;
 	}
