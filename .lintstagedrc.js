dd4a5356847574e1721638d25f2427612662898f
Gabriel <gabriel.serrano.salas@outlook.com>
2023-05-25 23:48:26 +0200

    chore: run prettier hook on all files, useful for .md runbooks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'force-app/**/lwc/**/*.js': 'import-sort --write',
-	'force-app/**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
+	'**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
 	'force-app/**/{aura,lwc}/**/*.js': 'eslint --fix',
 	'force-app/**/lwc/**': 'npm run test:unit:silent -- --passWithNoTests',
 	'force-app/**/*.{cls,trigger}': (filenames) => {
