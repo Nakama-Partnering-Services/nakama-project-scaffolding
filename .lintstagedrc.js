56fc6b4a4e352db1ed1719c165aac4bc6000ccad
Gabriel <gabriel.serrano.salas@outlook.com>
2023-05-25 23:48:26 +0200

    chore: run prettier hook on all files, useful for .md runbooks

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	'sfdx-source/**/lwc/**/*.js': 'import-sort --write',
-	'sfdx-source/**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
+	'**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
 	'sfdx-source/**/{aura,lwc}/**/*.js': 'eslint --fix',
 	'sfdx-source/**/lwc/**': 'npm run test:unit:silent -- --passWithNoTests',
 	'sfdx-source/**/*.{cls,trigger}': (filenames) => {
