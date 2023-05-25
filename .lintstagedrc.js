a1f5a7e27d2bb9d5fbda66f6d06885eddd451eac
Gabriel <gabriel.serrano.salas@outlook.com>
2023-05-25 23:29:28 +0200

    fix: lintstagedrc.js

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,9 +1,9 @@
 module.exports = {
 	'sfdx-source/**/lwc/**/*.js': 'import-sort --write',
 	'sfdx-source/**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
-	'sfdx-source/**/{aura,lwc}/**': 'eslint --fix',
-	// '**/lwc/**': 'npm run test:unit:silent .',
-	'**/*.{cls,trigger}': (filenames) => {
+	'sfdx-source/**/{aura,lwc}/**/*.js': 'eslint --fix',
+	'sfdx-source/**/lwc/**': 'npm run test:unit:silent -- --passWithNoTests',
+	'sfdx-source/**/*.{cls,trigger}': (filenames) => {
 		const files = filenames.join(',');
 		return `sfdx scanner:run --pmdconfig config/pmd-ruleset.xml --target "${files}" --engine pmd --format table --severity-threshold 3`;
 	}
