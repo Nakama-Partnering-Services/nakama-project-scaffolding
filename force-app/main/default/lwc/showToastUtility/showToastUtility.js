3ffdde16eb3146581fcef53d35853687a50d4640
Gabriel <gabriel.serrano.salas@outlook.com>
2023-06-15 19:16:34 +0200

    chore: indeed, it failed. Last verification with latest stable version 58

--- force-app/main/default/lwc/showToastUtility/showToastUtility.js
+++ force-app/main/default/lwc/showToastUtility/showToastUtility.js
@@ -46,7 +46,7 @@ const showToastError = (self, options) => {
 /**
  * Shows a warning toast notification
  * @param {object} self - The context (this) where you call the notification. Mandatory.
- * @param {object} options - Params to show the notification. Optional.
+ * @param {object} options - Params to show the notification. Optional
  *     - {string} message - Text to show in the notification
  *     - {string} title - Alternative title.
  *     - {string} mode - Determines how persistent the toast is. Possible values are: dismissable | pester | sticky
