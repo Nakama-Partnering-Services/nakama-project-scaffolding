783ef05a687fd81b4da12c4d1a4aa2973f3a4ed3
Gabriel <gabriel.serrano.salas@outlook.com>
2023-06-15 10:35:32 +0200

    chore: test github after touching image 3

--- force-app/main/default/lwc/showToastUtility/showToastUtility.js
+++ force-app/main/default/lwc/showToastUtility/showToastUtility.js
@@ -9,7 +9,7 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
  * Shows a success toast notification
  * @param {object} self - The context (this) where you call the notification. Mandatory.
  * @param {object} options - Params to show the notification. Optional
- *     - {string} message - Text to show in the notification
+ *     - {string} message - Text to show in the notification.
  *     - {string} title - Alternative title
  *     - {string} mode - Determines how persistent the toast is. Possible values are: dismissable | pester | sticky
  */
