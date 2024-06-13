7c349b86e82bf577a34b96fcfc146853417dfa94
jdkgabri <gabriel.serrano.salas@outlook.com>
2024-06-13 13:58:21 +0200

    chore: use yarn for run jest tests

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'**/*.{auradoc,cls,cmp,component,css,design,html,js,json,md,page,trigger,xml,yaml,yml}': 'prettier --write',
 	'force-app/**/{aura,lwc}/**/*.js': 'eslint --fix',
-	'force-app/**/lwc/**': 'npm run test:unit:silent -- --passWithNoTests',
+	'force-app/**/lwc/**': 'yarn test:unit:silent -- --passWithNoTests',
 	'force-app/**/*.{cls,trigger}': (filenames) => {
 		const files = filenames.join(',');
 		return `sf scanner run --pmdconfig config/pmd-ruleset.xml --target "${files}" --engine pmd --format table --severity-threshold 3`;
